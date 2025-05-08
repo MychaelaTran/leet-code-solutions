5e088786a8dfae9b882b9666901de670dd48d0f0
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-05-08 15:50:50 -0400

    Time: 0 ms (100%), Space: 17.8 MB (71.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,38 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        print(nums1)
-        print(nums2)
-        i = 0
-        j = 0    
-        total = 0
-        answer = [0] * (m + n)
-        while i < m and j < n:
-            #merge
-            if nums1[i] <= nums2[j]:
-                answer[total] = nums1[i]
-                total += 1
-                i += 1
-    
+        #need to merge from the back, to modify nums1 in place
+        i = m -1
+        j = n - 1
+        total = m + n - 1
+
+        while i >= 0  and j >=0: 
+            if nums1[i] >= nums2[j]:
+                nums1[total] = nums1[i]
+                total -= 1
+                i -= 1
             else: 
-                answer[total] = nums2[j]
-                total += 1
-                j += 1
+                nums1[total] = nums2[j]
+                total -= 1
+                j -= 1
         
+        while i >=0: 
+            nums1[total] = nums1[i]
+            total -= 1
+            i -= 1
         
-        while (i < m):
-            answer[total] = nums1[i]
-            i += 1
-            total += 1
-        
+        while j >= 0: 
+            nums1[total] = nums2[j]
+            total -= 1
+            j -= 1
 
-        while j < n:
-            answer[total] = nums2[j]
-            j += 1
-            total += 1
-
-        for i in range(m + n):
-            nums1[i] = answer[i]
 
 
         return nums1
