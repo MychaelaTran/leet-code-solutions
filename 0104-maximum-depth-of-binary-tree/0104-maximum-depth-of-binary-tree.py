2a761f872b6c6e17a3c0beefc9513c15b265f261
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-05-16 15:38:20 -0400

    Time: 0 ms (100%), Space: 19.1 MB (48.06%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -8,20 +8,38 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None: 
             return 0
-
-        if root.left is None and root.right is None: 
+        
+        if root.left is None and root.right is None:
             return 1
         
-        depth = 0
-        return self.helper(root)
+        height = 2
+        left = 0
+        right = 0
 
-    
+        if root.left is not None: 
+            left = self.helper(root.left, height)
 
-    def helper(self, node):
-        if node is None:
-            return 0
-        left = self.helper(node.left)
-        right = self.helper(node.right)
-        return 1 + max(left, right)
+        if root.right is not None: 
+            right = self.helper(root.right, height)
+        
+        return max(left, right)
 
 
+    def helper(self, root, height):
+        if root is not None:
+            if root.left is not None and root.right is not None: 
+                height += 1
+                return max(self.helper(root.left, height), self.helper(root.right , height))
+            
+            if root.left is not None:
+                height += 1
+                return self.helper(root.left, height)
+            
+            if root.right is not None:
+                height += 1
+                return self.helper(root.right, height)
+            
+            return height 
+        else: 
+            return height 
+        
