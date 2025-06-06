b3d1f5189b479035fa0ce7e8c05ae5d0aa9cc406
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-06 14:31:08 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        #if empty, returtn nothing 
+        if not digits:
+            return []
+
+        #hashing 
+        hash_map = {
+            "2": "abc", "3": "def", "4": "ghi",
+            "5": "jkl", "6": "mno", "7": "pqrs",
+            "8": "tuv", "9": "wxyz"
+        }
+
+        ans = []
+
+        def backtrack(index: int, curr_combo: str):
+            #add once at final figit 
+            if index == len(digits):
+                ans.append(current_combination)
+                return
+
+            #get digits and alll its letters
+            curr_digit = digits[index]
+            letters = hash_map[curr_digit]
+
+            #do each letter and then next digit 
+            for letter in letters:
+                backtrack(index + 1, curr_combo + letter)
+
+        #start backtacking 
+        backtrack(0, "")
+        return ans 
