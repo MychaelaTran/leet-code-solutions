00f384fbb4406e72393ae23f670ba35a04c13ac1
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-07 15:09:29 -0400

    Time: 0 ms (100%), Space: 17.8 MB (67.13%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        length = len(matrix)
+        #need to transpond and then reverse it's x 
+        print("orignal matrix ", matrix)
+        #transpod
+        for row in range(length):
+            for col in range(row):
+                matrix[row][col], matrix[col][row] =matrix[col][row], matrix[row][col]
+
+        print("After transpondL ", matrix)
+        #[list from 321 to 123]
+        for row in matrix:
+            row.reverse()
+        
+
+        return matrix
+
+
+
