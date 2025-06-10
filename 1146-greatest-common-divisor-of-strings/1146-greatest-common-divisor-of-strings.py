21c4ff623f21b441e500a00a34dafdfa1b32d2f2
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-10 16:11:53 -0400

    Time: 0 ms (100%), Space: 17.8 MB (71.18%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,46 +1,10 @@
+import math
+
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        ans = ""
-        #get shorter string 
-        st1_l = len(str1)
-        st2_l = len(str2)
-        print("len s1 ", st1_l )
-        print("len s2 ", st2_l )
-
-        if st2_l < st1_l:
-            iterator = st2_l
-            check1 = str2
-            check2 = str1
-        else: 
-            iterator = st1_l
-            check1 = str2
-            check2 = str1
-       
-
-        for i in (range(iterator + 1, 0, -1)): #start bbackwards until find 
-            temp_length = i 
-            print(temp_length)
-            if (len(check2) % temp_length == 0 and len(check1) % temp_length == 0) or temp_length == 1: 
-                temp = check1[:i]
-
-                #check str1
-                temp1 = ""
-                temp1_check = False
-                while len(temp1) <= len(str1):
-                    temp1 += temp
-                    if temp1 == str1:
-                        temp1_check = True
-                
-                #check str2
-                temp2 = ""
-                while len(temp2) <= len(str2):
-                    temp2 += temp
-                    if temp2 == str2:
-                        if temp1_check == True: 
-                            return temp
-                    
-                    
-            else:
-                continue
+        if str1 + str2 != str2 + str1:
+            return "" #no GCD exists, else would these woiuld be equial 
         
-        return ans
+        gcd_len = math.gcd(len(str1), len(str2))
+        
+        return str1[:gcd_len]
