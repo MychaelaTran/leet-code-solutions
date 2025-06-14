4c397baeb848f15070aa9596ab6cdfddfa9078bf
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-14 00:22:58 -0400

    Time: 22 ms (36.38%), Space: 37.5 MB (23.48%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,3 +1,22 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums) < 3: 
+            return False
+        small = 1000000000000
+        mid = 1000000000000
+
+        for num in nums:
+            if num > mid: 
+                return True
+            if num <= small: 
+                small = num
+            else:  #if num > small and num <= mid  
+                mid = num 
         
+        return False
+                    
+
+    #.  20  1000  10  12  5  13
+    #.  10  1000  5  12  20  13
+
+            
