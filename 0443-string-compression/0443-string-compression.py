6f1597cf328a1e93b3aea641ae247c93fc18c8f6
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-14 00:21:14 -0400

    Time: 3 ms (38.33%), Space: 18.1 MB (27.4%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,77 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = 0
+        read = 0
+        length = len(chars)
+        if length <= 1:
+            return length
+        
+
+        while read < length: 
+            curr = chars[read]
+            count = 0 
+
+            while read < length and chars[read] == curr: 
+                count += 1
+                read += 1
+            
+            chars[write] = curr
+            write += 1
+            if count > 1: 
+                for digit in str(count): 
+                    chars[write] = digit
+                    write += 1
+            
+            print("write num is ", write)
+            print("kength num is ", length)
+        
+        while write < len(chars): 
+            chars.pop()
+        
+        return len(chars)
+
+
+
+
+
+
+        # s = chars[0]
+        # seen = False
+        # curr_letter = chars[0]
+        # count  = 1
+        # ans = []
+        # #modify chars in placce 
+        # for i in range(1,len(chars)): 
+        #     #do normal 
+        #     if not seen: #new char looking at
+        #         if curr_letter == chars[i]: #conseccuitive repeating 
+        #             seen = True
+        #             curr_letter = chars[i]
+        #             count += 1
+        #             continue
+        #         else: 
+        #             curr_letter = chars[i]
+        #             s += curr_letter
+        #             count =  1
+        #             continue
+        #     else: 
+        #         if curr_letter == chars[i]: 
+        #             count += 1
+        #             continue
+        #         else: #new letter now
+        #             s += str(count)
+        #             count = 1 #reset count 
+        #             curr_letter = chars[i]
+        #             seen = False
+        #             s += curr_letter
+        
+        # if seen == True: 
+        #     s += str(count)
+
+        # for i in range(len(chars)):
+            
+
+
+        # print("this is s ", s)
+        # return len(ans)
+        
