fba1fbdbbe53fc384e03bdeb050e008f402edb80
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-15 20:00:12 -0400

    Time: 255 ms (5.06%), Space: 21.9 MB (7.61%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        #only delete one elment 
+        left = 0
+        ans = 0
+        temp = 0
+        used = False
+        for right in range(len(nums)): 
+            if nums[right] == 1: 
+                temp += 1
+                print("enterted when right is ", right)
+                print("temp uis ", temp)
+                continue
+            else:
+                if not used: 
+                    used = True
+                    continue
+                else: 
+                    if temp > ans: 
+                        ans = temp
+                    while nums[left] != 0: 
+                        left += 1
+                        temp -= 1
+                    left += 1
+                    used = True
+
+        if temp > ans and used: 
+            return temp
+        
+        if not used: 
+            return temp -1
+        
+        return ans
+                
+                
+        
+
+        return ans
+
+        
