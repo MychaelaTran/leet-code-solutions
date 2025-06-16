55b7573088df667a6a1fe046069c31adcc4ba9a7
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-15 20:02:49 -0400

    Time: 23 ms (95.61%), Space: 21.8 MB (67.84%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,38 +1,35 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         #only delete one elment 
+        #sliding window
         left = 0
         ans = 0
         temp = 0
         used = False
         for right in range(len(nums)): 
             if nums[right] == 1: 
-                temp += 1
+                temp += 1 #add to count when see 1
                 continue
             else:
-                if not used: 
+                if not used: #if havent deleted one, can delete and continue
                     used = True
                     continue
-                else: 
+                else:  #alr deleted a 0
                     if temp > ans: 
-                        ans = temp
-                    while nums[left] != 0: 
+                        ans = temp #update ans
+                    while nums[left] != 0:  #move windown until we hit a zero to be able to delete a 0 again
                         left += 1
-                        temp -= 1
-                    left += 1
-                    used = True
+                        temp -= 1 #minus 1 each time bc this is moving pass 1's
+                    left += 1 #move the left up not because done dealing wioth current 0
+                    used = True #and have used our flip 
 
         if temp > ans and used: 
             return temp
         
-        if not used: 
+        if not used:  #if never flipped still need to delet eone
             return temp -1
         
         return ans
                 
-                
-        
-
-        return ans
 
         
