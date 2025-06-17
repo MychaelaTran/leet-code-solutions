1595409b0a3d283801d470cb7593ac5a4cadadc2
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-17 00:49:52 -0400

    Time: 47 ms (42.41%), Space: 22.1 MB (19.56%) - LeetHub

---
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        ans = 0
+        rows = {}
+        cols = {}
+
+        #hash tuples for row 
+        for row in range(len(grid)): 
+            row_tuple = tuple(grid[row])  #makign row into tuple
+            print("this is row_tiple ", row_tuple)
+            if row_tuple not in rows: 
+                rows[row_tuple] = 1
+            else:
+                rows[row_tuple] += 1
+
+        #hash tuples for cols
+        for col in range(len(grid[0])): 
+            col_tuple = tuple(grid[row][col] for row in range(len(grid)))  
+            if col_tuple not in cols: 
+                cols[col_tuple] = 1
+            else: 
+                cols[col_tuple] += 1
+
+        #get mathching pairs bt multiplying 
+        for key in rows:
+            if key in cols:
+                ans += rows[key] * cols[key]
+
+        return ans
