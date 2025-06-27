24beb1bf07c8ae8fd0a88fe8e94d22de5b0ea552
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-27 00:20:39 -0400

    Time: 27 ms (5.06%), Space: 18 MB (94.03%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -0,0 +1,52 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        #use DFS
+        self.keys = []
+        self.stack = [0]
+        self.visited = []
+
+        def dfs(node): 
+            while self.stack: 
+                curr = self.stack.pop()
+                if curr not in self.visited: 
+                    self.visited.append(curr)
+                for i in range(len(rooms[curr])): 
+                    if rooms[curr][i] not in self.keys: 
+                        self.keys.append(rooms[curr][i]) 
+                        self.stack.append(rooms[curr][i])
+            return
+
+
+        dfs(rooms[0])
+        print(self.visited)
+        return len(self.visited) == len(rooms)
+
+
+
+'''
+ rooms = [[1],[2],[3],[]]
+ keys = []
+ stack = [0]
+ visted = []
+
+ 1. curr = 0
+ visited = [0]
+ i = 1
+ rooms[0][0]
+ stack = [1]
+ keys = [1]
+
+ 2. 
+ curr = 1
+ visited = [0,1]
+ keys = [1,2]
+ stack = [2]
+
+ 3.
+
+ stack = [3]
+ keys = [1,2,3]
+
+
+
+ '''
