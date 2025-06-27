ce8af173afc1a99c1750d405b8fd8f96db3e0800
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-06-27 16:27:24 -0400

    Time: 7 ms (56.95%), Space: 19 MB (70.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -5,7 +5,7 @@ class Solution:
         provinces = 0
 
         for i in range(n):
-            if i not in seen:
+            if i not in seen: #have this for when done searchign a province you have to search the next
                 stack = [i]
                 while stack:
                     curr = stack.pop()
@@ -14,6 +14,6 @@ class Solution:
                         for j in range(n):
                             if isConnected[curr][j] == 1 and j not in seen:
                                 stack.append(j)
-                provinces += 1
+                provinces += 1 #add provinve when done one break of while loop
 
         return provinces
