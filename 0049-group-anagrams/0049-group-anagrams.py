0685aed84d1c1c5d2bc840b809ef845a65747782
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-07-14 01:53:58 -0400

    Time: 19 ms (37.02%), Space: 21.5 MB (37.93%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 1:
+            return [strs]
+        
+        ans =[]
+        
+        myhash = defaultdict(list)
+        
+        for word in strs: 
+            sortedWord = ''.join(sorted(word))    
+            myhash[sortedWord].append(word)
+        
+        print(myhash)
+        for key in myhash.keys(): 
+            temp  = []
+            for value in myhash[key]: 
+                temp.append(value)
+            ans.append(temp)
+            
+        return ans
