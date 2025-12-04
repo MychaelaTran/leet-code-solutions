ebb1a64a0fdb010a1d2de4b7d889a9478ee010da
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-12-04 12:39:16 -0500

    Time: 26 ms (5.72%), Space: 18.1 MB (14.27%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,44 +5,54 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = []
-        num2 = []
-        curr1 = l1
-        curr2 = l2
-        print("this is l1", l1)
-        #while loops to make the LL into list 
-        while curr1 is not None:
-            num1.append(curr1.val)
-            curr1 = curr1.next
+        p1 = l1
+        p2 = l2
+        ans = ListNode()
+        head = ans
+        carry = 0
 
-        while curr2 is not None:
-            num2.append(curr2.val)
-            curr2 = curr2.next
-
-        #reverse them 
-        num1.reverse()
-        num2.reverse()
-
-        num1r = int("".join((map(str, num1))))  #use map to make into string then join them together using join, sperating thjem withj nothing ""
-        num2r = int("".join((map(str, num2))))
-        result = num1r + num2r
-    
-        #make result back inti list
-        list_str = list(str(result))
-        list_str.reverse() #reverse
-        answer = list(map(int, list_str)) #maps all to int and then need to wrap in list bc map returns an obj so make returnm lsit
-        print(answer)
-        #make back into type ListNode
-        list_node = ListNode()
-        curr = list_node
-        for i in range(len(answer)): 
-            if i == len(answer) -1:
-                curr.val = answer[i]
+        while p1 is not None or p2 is not None: 
+            if p1 is None: 
+                p1_val = 0
             else: 
-                curr.val = answer[i]
-                curr.next = ListNode()
-                curr = curr.next
-        print("this is answer", list_node)
-        return list_node
+                p1_val = p1.val
+            
+            if p2 is None: 
+                p2_val = 0
+            else: 
+                p2_val = p2.val
+
+            digit = p1_val + p2_val
+            print("this is difit: ", digit)
+            ans.val = (digit + carry) % 10
+            carry = (digit + carry) // 10
+
+            if p1: 
+                p1 = p1.next
+            if p2: 
+                p2 = p2.next
+
+            if not p1 and not p2: 
+                if carry == 0:
+                    break
+                else: 
+                    ans.next = ListNode()
+                    ans.next.val = carry
+                    break
 
 
+
+            ans.next = ListNode()
+            ans = ans.next
+            
+        
+        
+        print(p1)
+        print(p2)
+                
+        
+        print(head)
+
+        return head
+
+        
