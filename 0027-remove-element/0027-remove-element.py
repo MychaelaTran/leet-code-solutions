25a3b15736cf0e54f33d4b79d72a36cb757b3beb
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-12-05 17:23:05 -0500

    Time: 0 ms (100%), Space: 17.7 MB (51.98%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,17 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != val: 
-                nums[k] = nums[i]
-                k += 1
-        return k
+        i = 0
+        n = len(nums) 
         
+        while i < n:
+            if nums[i] == val:
+                #overwrite the bad elemmetn with last element 
+                nums[i] = nums[n - 1]
+                #"delete" the invalid spot
+                n -= 1
+                #dont increment i because we need to checkl if the number we just swapped with is valid 
+            else:
+                #if not val then can move to next element 
+                i += 1
+                
+        return n
