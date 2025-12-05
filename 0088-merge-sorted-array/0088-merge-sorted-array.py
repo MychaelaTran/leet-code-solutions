c26bf4706d0fe0afaa7f2d5b9f123337780ba774
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-12-05 17:05:55 -0500

    Time: 0 ms (100%), Space: 17.7 MB (71.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,25 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # Pointers for nums1 and nums2
+        #ptrs for nums1 and nums2
         p1 = m - 1
-        p2 = n - 1
-        
-        # Pointer for the end of the merged array
-        p = m + n - 1
-        
-        # While there are elements to be processed in nums2
-        while p2 >= 0:
-            # If nums1 still has elements and nums1's element is larger
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                # Otherwise, take from nums2
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            # Move the write pointer backwards
-            
-            p -= 1
+        p2 = n -1
+        total = len(nums1) -1
 
-        return nums1
+        while total >= 0 and p1 >= 0 and p2 >= 0: 
+            if nums1[p1] > nums2[p2]: 
+                nums1[total] = nums1[p1]
+                p1 -= 1
+                total -=1
+                
+            else: 
+                 nums1[total] = nums2[p2]
+                 p2 -= 1 
+                 total -= 1
+        
+        while p1 >= 0: 
+            nums1[total] = nums1[p1]
+            total -= 1
+            p1 -= 1
+
+        while p2 >= 0: 
+            nums1[total] = nums2[p2]
+            total -= 1
+            p2 -= 1
+
+       
