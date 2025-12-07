eb5e76d57bda5252ed0867bb2695ec15a46732fa
Mychaela Tran <154871117+MychaelaTran@users.noreply.github.com>
2025-12-07 09:14:49 -0500

    Time: 316 ms (50.63%), Space: 17.9 MB (59.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,27 +1,33 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        #use a 
-        res = ""
-        long_len = 0
-        
-        #check odd length palindromes
-        for i in range(len(s)):
-            l, r = i, i #center pos
-            while l >= 0 and r < len(s) and s[r] == s[l]: 
-                if (r-l + 1) > long_len: 
-                    long_len = r-l + 1
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
-            
-            #even length palindromes: 
-            l, r = i, i+ 1
-            while l >= 0 and r < len(s) and s[r] == s[l]: 
-                if (r-l + 1) > long_len: 
-                    long_len = r-l + 1
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
-        return res
+        #every character is a palindome 
+        #if odd palindrome, middle char no match
+        #even lemgth palindrom eacj same
 
+        res = ""
+        longest = 0
+
+        #check odd length
+        for i in range(len(s)): 
+            l, r = i, i #center ptrs
+
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                temp = r - l +1
+                if temp > longest: 
+                    res = s[l:r+1]
+                    longest = temp
+                l-= 1
+                r+= 1
         
+        #even length palindoems 
+        for i in range(len(s)):
+            l, r = i, i +1
+            while l >=0 and r < len(s) and s[l] == s[r]:
+                temp = r - l +1
+                if temp > longest: 
+                    res = s[l:r+1]
+                    longest = temp
+                l-= 1
+                r+= 1
+        
+        return res
